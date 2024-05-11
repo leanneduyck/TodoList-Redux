cfc7b0bf7f4829e78e5fe69aa300daaa8b8f0ac7
Leanne <leanne.m.dk@gmail.com>
2024-05-10 22:11:41 -0700

    bug fixes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import MainView from './components/main-view.jsx';
+import MainView from './components/main-view';
 import store from './store.js';
 
 // renders MainView (TodoList)
 // wrapped in Provider, kept getting errors from the store w/Container
 const App = () => {
   return (
-    <Provider store={store}>
-      <MainView class="main-view" />
-    </Provider>
+    // <Provider store={store}>
+    <MainView class="main-view" />
+    // </Provider>
   );
 };
 
